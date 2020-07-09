406601a6d93f2c010d4d3c087c9be3c926b70457
CodePallavi <mailittopallavi@gmail.com>
2020-07-10 01:51:35 +0800

    code clean up

--- src/app/service/datasvc.service.ts
+++ src/app/service/datasvc.service.ts
@@ -13,16 +13,19 @@ export class DatasvcService {
   private dataURL: string = 'https://jsonplaceholder.typicode.com/posts';
  // private commentsURL: 'https://jsonplaceholder.typicode.com/comments?postId='
   constructor(private http: HttpClient) {}
-
+  
+  // Function to get all posts
   public getPosts() {
 
     return this.http.get(this.dataURL);
   }
-
+  
+  // Function to get posts by id
   public getPostsById(id){
     return this.http.get(this.dataURL+'/'+id)
   }
-
+  
+  // Function to get comments for a post by Id
   public getCommentsByPost(id){
     return this.http.get('https://jsonplaceholder.typicode.com'+'/comments?postId='+id)
   }
--- src/app/show-post/show-post.component.ts
+++ src/app/show-post/show-post.component.ts
@@ -4,40 +4,34 @@ import { DatasvcService } from '../service/datasvc.service';
 @Component({
   selector: 'app-show-post',
   templateUrl: './show-post.component.html',
-  styleUrls: ['./show-post.component.scss']
+  styleUrls: ['./show-post.component.scss'],
 })
 export class ShowPostComponent implements OnInit {
-
   data = {
-    postId:''
-  }
+    postId: '',
+  };
 
   searchData = {
-    email:'',
-    name:'',
-    body:''
-  }
+    email: '',
+    name: '',
+    body: '',
+  };
 
   public currId: any;
   public posts: any;
   public showCommBool: boolean = false;
-  public comments : any ;
+  public comments: any;
   public srcEmail: any;
   public srcName: any;
   public srcBody: any;
   public notSearched: boolean = false;
   public searchedComm: any;
 
+  constructor(private srvc: DatasvcService) {}
 
-  constructor( private srvc: DatasvcService) { }
+  ngOnInit() {}
 
-  
- 
-
-  ngOnInit(){
-  }
-
-  searchPost(){
+  searchPost() {
     this.currId = this.data.postId;
     this.srvc.getPostsById(this.currId).subscribe((res) => {
       console.log(res);
@@ -45,22 +39,16 @@ export class ShowPostComponent implements OnInit {
     });
   }
 
-  
-
-  showComments(){
-    this.currId =  this.data.postId;
-    this.srvc.getCommentsByPost(this.currId).subscribe((res) =>{
-      this.showCommBool = true 
+  showComments() {
+    this.currId = this.data.postId;
+    this.srvc.getCommentsByPost(this.currId).subscribe((res) => {
+      this.showCommBool = true;
       console.log(res);
       this.comments = res;
-    })
+    });
   }
 
-
-  searchComment(){
-
-
-
+  searchComment() {
     this.srcEmail = this.searchData.email;
     this.srcName = this.searchData.name;
     this.srcBody = this.searchData.body;
@@ -72,32 +60,22 @@ export class ShowPostComponent implements OnInit {
     let commentsArrayLength = this.comments.length;
 
     this.notSearched = true;
-  //  let i =0;
-   for(let i = 0; i<commentsArrayLength; i++){
-
+    //  let i =0;
+    for (let i = 0; i < commentsArrayLength; i++) {
       emailArr[i] = this.comments[i].email;
       nameArr[i] = this.comments[i].name;
-      bodyArr[i] =this.comments[i].body;
-    
-
+      bodyArr[i] = this.comments[i].body;
     }
 
-    for(let k=0; k<emailArr.length; k++){
-    if (this.srcEmail === emailArr[k]){
-      this.searchedComm = this.comments[k];
-      console.log(this.searchedComm);
-    } else if (this.srcName === nameArr[k]){
-      this.searchedComm = this.comments[k];
-    } else if (this.srcBody === bodyArr[k]){
-      this.searchedComm = this.comments[k];
+    for (let k = 0; k < emailArr.length; k++) {
+      if (this.srcEmail === emailArr[k]) {
+        this.searchedComm = this.comments[k];
+        console.log(this.searchedComm);
+      } else if (this.srcName === nameArr[k]) {
+        this.searchedComm = this.comments[k];
+      } else if (this.srcBody === bodyArr[k]) {
+        this.searchedComm = this.comments[k];
+      }
     }
   }
-    
-  }
-
-  
- 
-
-
-
 }
